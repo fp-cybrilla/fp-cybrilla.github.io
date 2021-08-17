646bf1315b2d40ede24ac2f86f0541ea0a18afb1
Prashant <prashant@cybrilla.com>
2021-08-17 17:43:59 +0530

    Layout and check KYC page changes

--- content/identity/kyc-check.md
+++ content/identity/kyc-check.md
@@ -1,8 +1,7 @@
-# KYC Check
-*Check your investor's kyc status*
+## KYC Check
+#### Check your investor's kyc status
 
 Use this api to check if your investor's identity detail exists in any of the KRAs (KYC Registration Agencies) database. If it exists, you'll get certain demographic information of the investor and the status of the KYC in the response object.
-
 ### Check kyc status
 Call the [KYC Check api](https://fintechprimitives.com/api/#create-a-kyc-check) with the investor's Income Tax Permanent Account Number (PAN)
 ```json
@@ -10,14 +9,12 @@ Call the [KYC Check api](https://fintechprimitives.com/api/#create-a-kyc-check) 
 	"pan": "arrpp1115n"
 }
 ```
-*Status*  
+#### Status  
 If the `status` in the response object is `true`, it means the investor details exists in one of the KRA's databases and his KYC is under process or completed successfully. You can go ahead and onboard the investor and place purchase and redemption orders.
 
 If the `status` is `false`, it means the investor either does not exist in any of the KRA's databases or his KYC is unsuccessful. We recommend that you do not accept any orders from the investor at this stage, as they will get rejected during the processing at the RTA.
-
-*Investor Details*  
+#### Investor Details
 The `entity_details` in the response object will contain the investor's demographic information. In most cases, you'll get the `name` of the investor.
-
 ### Testing
 To test various scenarios in your application during the development process, use the following pan numbers in the staging environment.
 
---
+++ content/pixel.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Modal = function () {
+  function Modal(id) {
+    _classCallCheck(this, Modal);
+
+    if (id) {
+      this.elem = document.getElementById(id);
+      this.elem.classList.add('hidden');
+    }
+  }
+
+  _createClass(Modal, [{
+    key: 'show',
+    value: function show(callback) {
+      if (this.elem.classList.contains('hidden')) {
+        this.elem.classList.remove('hidden');
+        this.elem.classList.add('block');
+      }
+
+      if (callback) callback();
+    }
+  }]);
+
+  return Modal;
+}();
+
+var Collapse = function () {
+  function Collapse() {
+    _classCallCheck(this, Collapse);
+
+    this.ele = document.querySelectorAll('.collapse-title');
+    this.toggle();
+  }
+
+  _createClass(Collapse, [{
+    key: 'toggle',
+    value: function toggle() {
+      this.ele.forEach(function (item, key) {
+        var nextEle = item.nextElementSibling;
+        var iconElement = item.querySelector('.collapse-icon');
+        iconElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><g transform="translate(0 16) rotate(-90)"><g transform="translate(1.41 4.022)"><rect class="a" fill="#0f2c5e" width="1.667" height="9.575" rx="0.833" transform="translate(0 1.179) rotate(-45)"/><rect class="a" fill="#0f2c5e" width="1.667" height="9.575" rx="0.833" transform="translate(6.761 7.949) rotate(-135)"/></g><rect class="b" fill="none" width="16" height="16" transform="translate(0 0)"/></g></svg>';
+
+        item.addEventListener('click', function () {
+          if (nextEle.classList.contains('hidden')) {
+            nextEle.classList.remove('hidden');
+            nextEle.classList.add('block');
+            iconElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><g transform="translate(0 0)"><g transform="translate(1.41 4.022)"><rect class="a" fill="#0f2c5e" width="1.667" height="9.575" rx="0.833" transform="translate(0 1.179) rotate(-45)"/><rect class="a" fill="#0f2c5e" width="1.667" height="9.575" rx="0.833" transform="translate(6.761 7.949) rotate(-135)"/></g><rect class="b" fill="none" width="16" height="16" transform="translate(0 0)"/></g></svg>';
+          } else {
+            nextEle.classList.remove('block');
+            nextEle.classList.add('hidden');
+            iconElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><g transform="translate(0 16) rotate(-90)"><g transform="translate(1.41 4.022)"><rect class="a" fill="#0f2c5e" width="1.667" height="9.575" rx="0.833" transform="translate(0 1.179) rotate(-45)"/><rect class="a" fill="#0f2c5e" width="1.667" height="9.575" rx="0.833" transform="translate(6.761 7.949) rotate(-135)"/></g><rect class="b" fill="none" width="16" height="16" transform="translate(0 0)"/></g></svg>';
+          }
+        });
+      });
+    }
+  }]);
+
+  return Collapse;
+}();
