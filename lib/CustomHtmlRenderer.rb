fe7d89942b7ce952a2441379ddba8e9defbde5a1
Prashant <prashant@cybrilla.com>
2021-08-19 15:20:00 +0530

    Table stying

--- lib/CustomHtmlRenderer.rb
+++ lib/CustomHtmlRenderer.rb
@@ -4,4 +4,11 @@ require 'rouge/plugins/redcarpet'
 
 class Redcarpet::Render::CustomHTML < Redcarpet::Render::HTML
   include Rouge::Plugins::Redcarpet
+
+  def table(header, body)
+    "<table class='table-default table-auto'>" \
+      "<thead>#{header}</thead>" \
+      "<tbody>#{body}</tbody>" \
+    "</table>"
+  end
 end
